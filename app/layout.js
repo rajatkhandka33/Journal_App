35eebf0ec801a8a4740eb95f9f3e58f0ea455c0e
Rajat Khandka <rajatkhandka33@gmail.com>
2025-06-18 00:39:00 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -42,7 +42,7 @@ export default function RootLayout({ children }) {
 
           <footer className="bg-orange-300 py-12 bg-opacity-10">
             <div className="container mx-auto px-4 text-center text-gray-900">
-              <p>Made with 💗 by Rajat Khandka</p>
+              <p>Made with 💗</p>
             </div>
           </footer>
         </body>
